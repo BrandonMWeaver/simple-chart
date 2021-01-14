1857d7a23fa7de12d86b50a27d54eba82d5c1e5c
Brandon Weaver <a7xncob@gmail.com>
2021-01-14 14:08:09 -0500

    Add simple chart constructor, append, set color, set size, and draw methods.

--- SimpleChart.js
+++ SimpleChart.js
@@ -1,1 +1,43 @@
-class SimpleChart {}
+class SimpleChart {
+    constructor(x1, x2, y1, y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+
+        this.canvas = document.createElement("canvas");
+        this.context = this.canvas.getContext("2d");
+
+        this.container = null;
+        this.color = null;
+    }
+
+    append = container => {
+        this.container = container;
+
+        this.container.append(this.canvas);
+
+        this.#setSize();
+
+        window.addEventListener("resize", this.#setSize);
+    }
+
+    setColor = color => {
+        this.color = color;
+        this.#draw();
+    }
+
+    #setSize = () => {
+        this.canvas.style.width = "100%";
+        this.canvas.style.height = "100%";
+        this.canvas.width = this.canvas.offsetWidth;
+        this.canvas.height = this.canvas.offsetHeight;
+
+        this.setColor(this.color);
+    }
+
+    #draw = () => {
+        this.context.fillStyle = this.color;
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
