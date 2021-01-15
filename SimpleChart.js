aa1b1dc66d0e8701a95502d0f83517a2ea4dc487
Brandon Weaver <a7xncob@gmail.com>
2021-01-15 12:56:43 -0500

    Implement comparable data sets.

--- SimpleChart.js
+++ SimpleChart.js
@@ -4,70 +4,75 @@ class SimpleChart {
         this.context = this.canvas.getContext("2d");
 
         this.container = null;
-        this.data = null;
+        this.dataSet = [];
         this.color = "#fff";
     }
 
     append = container => {
         this.container = container;
-
         this.container.append(this.canvas);
 
+        this.setColor(this.color);
         this.#setSize();
 
         window.addEventListener("resize", this.#setSize);
     }
 
-    setData = data => {
-        this.data = data;
-        this.#drawData();
+    addData = data => {
+        this.dataSet.push(data);
+        this.#setSize();
     }
 
     setColor = color => {
         this.color = color;
-        this.#draw();
     }
 
     #setSize = () => {
         this.canvas.style.width = "100%";
         this.canvas.style.height = "100%";
-        this.canvas.width = this.canvas.offsetWidth;
-        this.canvas.height = this.canvas.offsetHeight;
-
-        this.setColor(this.color);
+        this.canvas.width = this.canvas.offsetWidth * 2;
+        this.canvas.height = this.canvas.offsetHeight * 2;
+        this.#draw();
     }
 
     #draw = () => {
         this.context.fillStyle = this.color;
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
-
-        if (this.data) this.#drawData();
+        
+        this.#drawData();
     }
 
     #drawData = () => {
-        let x = 0;
-        const xStep = this.canvas.width / (this.data.length - 1);
-
         let yLow = Number.MAX_VALUE;
         let yHigh = Number.MIN_VALUE;
-        for (const datum of this.data) {
-            if (datum < yLow) yLow = datum;
-            if (datum > yHigh) yHigh = datum;
+        for (const data of this.dataSet) {
+            for (const datum of data.arr) {
+                if (datum < yLow) yLow = datum;
+                if (datum > yHigh) yHigh = datum;
+            }
         }
 
-        for (let i = 0; i < this.data.length; i++) {
-            let yPercentage = (this.data[i] - yLow) / (yHigh - yLow) * 100;
+        for (const data of this.dataSet) {
+            let x = 0;
+            const xStep = this.canvas.width / (data.arr.length - 1);
+
+            let yPercentage = (data.arr[0] - yLow) / (yHigh - yLow) * 100;
             let y = this.canvas.height - this.canvas.height / 100 * yPercentage;
 
-            this.context.moveTo(x, y);
-            x += xStep;
+            this.context.strokeStyle = data.color;
+            this.context.beginPath();
 
-            if (i + 1 < this.data.length) {
-                let yPercentage = (this.data[i + 1] - yLow) / (yHigh - yLow) * 100;
-                let y = this.canvas.height - this.canvas.height / 100 * yPercentage;
+            for (let i = 0; i < data.arr.length; i++) {
+                this.context.moveTo(x, y);
+                x += xStep;
 
-                this.context.lineTo(x, y);
-                this.context.stroke();
+                if (i + 1 < data.arr.length) {
+                    yPercentage = (data.arr[i + 1] - yLow) / (yHigh - yLow) * 100;
+                    y = this.canvas.height - this.canvas.height / 100 * yPercentage;
+                    
+                    this.context.lineTo(x, y);
+                    this.context.stroke();
+                }
             }
         }
     }
