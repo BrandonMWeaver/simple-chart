e56886b055005c619fba826e4fff6978f1ecbe5b
Brandon Weaver <a7xncob@gmail.com>
2021-01-25 16:58:23 -0500

    Add simple chart SVG path mode and refactor.

--- SimpleChart.js
+++ SimpleChart.js
@@ -1,15 +1,17 @@
 class SimpleChart {
     constructor(type = "canvas") {
+        this.type = type;
         this.canvas = null;
         this.context = null;
-        if (type === "canvas") {
+
+        if (this.type === "canvas") {
             this.canvas = document.createElement("canvas");
             this.context = this.canvas.getContext("2d");
         }
-        else if (type === "svg") {
+        else if (this.type.split('-')[0] === "svg") {
             this.canvas = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         }
-        else throw new Error("Invalid chart type\nChart type must be either canvas or svg");
+        else throw new Error("Invalid chart type\nChart type must be canvas, svg-line, or svg-path");
 
         this.container = null;
         this.dataSet = [];
@@ -31,7 +33,7 @@ class SimpleChart {
 
     addData = data => {
         this.dataSet.push(data);
-        this.#setSize();
+        this.#draw();
     }
 
     setColor = color => {
@@ -66,8 +68,8 @@ class SimpleChart {
             this.canvas.height = this.canvas.offsetHeight * 2;
         }
         else {
-            this.canvas.setAttributeNS(null, "width", "100%");
-            this.canvas.setAttributeNS(null, "height", "100%");
+            this.canvas.setAttribute("width", "100%");
+            this.canvas.setAttribute("height", "100%");
         }
         this.#draw();
     }
@@ -80,61 +82,103 @@ class SimpleChart {
         else {
             this.canvas.innerHTML = '';
             const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-            rect.setAttributeNS(null, "width", "100%");
-            rect.setAttributeNS(null, "height", "100%");
-            rect.setAttributeNS(null, "fill", this.color);
+            rect.setAttribute("width", "100%");
+            rect.setAttribute("height", "100%");
+            rect.setAttribute("fill", this.color);
             this.canvas.append(rect);
         }
         
-        this.#drawData();
+        if (this.type === "canvas")
+            this.#drawData();
+        else if (this.type === "svg-line")
+            this.#drawDataLine();
+        else
+            this.#drawDataPath();
     }
 
     #drawData = () => {
-        let [yLow, yHigh] = this.getMinMax();
+        const [yLow, yHigh] = this.getMinMax();
+        const [width, height] = [this.canvas.width - this.strokeWidth, this.canvas.height - this.strokeWidth];
 
         for (const data of this.dataSet) {
-            let line = null;
-
-            let [width, height] = this.context ? [this.canvas.width, this.canvas.height] : [this.canvas.getBBox().width, this.canvas.getBBox().height];
-            width -= this.strokeWidth;
-            height -= this.strokeWidth;
-
             const xStep = width / (data.arr.length - 1);
 
             let x = 0 + this.strokeWidth / 2;
             let y = this.#getY(data.arr[0], height, yLow, yHigh);
 
-            if (this.context) {
-                this.context.strokeStyle = data.color;
-                this.context.beginPath();
-            }
+            this.context.strokeStyle = data.color;
+            this.context.beginPath();
 
             for (let i = 1; i < data.arr.length; i++) {
-                if (this.context)
-                    this.context.moveTo(x, y);
-                else {
-                    line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-                    line.setAttribute("x1", x);
-                    line.setAttribute("y1", y);
-                }
-                x += xStep;
-
-                y = this.#getY(data.arr[i], height, yLow, yHigh);
+                this.context.moveTo(x, y);
                 
-                if (this.context) {
-                    this.context.lineTo(x, y);
-                    this.context.lineWidth = this.strokeWidth;
-                    this.context.lineCap = "round";
-                    this.context.stroke();
-                }
-                else {
-                    line.setAttribute("x2", x);
-                    line.setAttribute("y2", y);
-                    line.setAttribute("stroke", data.color);
-                    line.setAttribute("stroke-width", this.strokeWidth);
-                    line.setAttribute("stroke-linecap", "round");
-                    this.canvas.append(line);
-                }
+                x += xStep;
+                y = this.#getY(data.arr[i], height, yLow, yHigh);
+
+                this.context.lineTo(x, y);
+                this.context.lineWidth = this.strokeWidth;
+                this.context.lineCap = "round";
+                this.context.stroke();
+            }
+        }
+    }
+
+    #drawDataPath = () => {
+        const [yLow, yHigh] = this.getMinMax();
+        const [width, height] = [this.canvas.getBBox().width - this.strokeWidth, this.canvas.getBBox().height - this.strokeWidth];
+
+        for (const data of this.dataSet) {
+            let x = 0 + this.strokeWidth / 2;
+            let y = this.#getY(data.arr[0], height, yLow, yHigh);
+
+            const xStep = width / (data.arr.length - 1);
+
+            const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+            path.setAttribute("fill", "#fff0");
+            path.setAttribute("stroke", data.color);
+            path.setAttribute("stroke-width", this.strokeWidth);
+            path.setAttribute("stroke-linecap", "round");
+
+            let pathCoordinates = `M ${x},${y}`;
+
+            for (let i = 1; i < data.arr.length; i++) {
+                x += xStep;
+                y = this.#getY(data.arr[i], height, yLow, yHigh);
+
+                pathCoordinates = `${pathCoordinates} L ${x},${y}`;
+            }
+
+            path.setAttribute("d", pathCoordinates);
+
+            this.canvas.append(path);
+        }
+    }
+
+    #drawDataLine = () => {
+        const [yLow, yHigh] = this.getMinMax();
+        const [width, height] = [this.canvas.getBBox().width - this.strokeWidth, this.canvas.getBBox().height - this.strokeWidth];
+
+        for (const data of this.dataSet) {
+            let x = 0 + this.strokeWidth / 2;
+            let y = this.#getY(data.arr[0], height, yLow, yHigh);
+
+            const xStep = width / (data.arr.length - 1);
+
+            for (let i = 1; i < data.arr.length; i++) {
+                const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+                line.setAttribute("x1", x);
+                line.setAttribute("y1", y);
+
+                x += xStep;
+                y = this.#getY(data.arr[i], height, yLow, yHigh);
+
+                line.setAttribute("x2", x);
+                line.setAttribute("y2", y);
+
+                line.setAttribute("stroke", data.color);
+                line.setAttribute("stroke-width", this.strokeWidth);
+                line.setAttribute("stroke-linecap", "round");
+                this.canvas.append(line);
             }
         }
     }
